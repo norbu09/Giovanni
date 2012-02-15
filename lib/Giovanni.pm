b7e9159cff68eb64c52709b9ea36f71952b195ee
Lenz Gschwendtner <lenz@springtimesoft.com>
2012-02-15 13:34:51 +1300

    Checking in changes prior to tagging of version 1.2.
    Changelog diff is:
    
    diff --git c/Changes w/Changes
    index 5a204b4..93643a7 100644
    --- c/Changes
    +++ w/Changes
    @@ -1,5 +1,9 @@
     Revision history for Giovanni
    
    +1.2     2012-02-15T13:34:19
    +        fixed bug with multiple adresses in the jabber notify plugin
    +        [lenz]
    +
     1.1     2012-02-13T20:58:16
             added some basic documentation

--- lib/Giovanni.pm
+++ lib/Giovanni.pm
@@ -16,11 +16,11 @@ Giovanni - a Perl based deployment system
 
 =head1 VERSION
 
-Version 1.1.0
+Version 1.2.1.0
 
 =cut
 
-our $VERSION = '1.1';
+our $VERSION = '1.2';
 
 has 'debug' => (
     is        => 'rw',
