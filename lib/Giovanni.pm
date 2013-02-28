c6c3d66a18c97775ba2b72c4153e58e7050bfc02
Lenz Gschwendtner <lenz@springtimesoft.com>
2013-02-28 13:05:51 +1300

    Checking in changes prior to tagging of version 1.7.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0e87176..b215c7e 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Giovanni
    
    +1.7     2013-02-28T13:04:44
    +        - removed XMPP dependency as it is horrible to install
    +        - added LWP::UserAgent for the HipChat plugin
    +
     1.6     2012-08-09T16:05:19
             added restart as a separate function

--- lib/Giovanni.pm
+++ lib/Giovanni.pm
@@ -16,11 +16,11 @@ Giovanni - a Perl based deployment system
 
 =head1 VERSION
 
-Version 1.4.4.5.5.4.4
+Version 1.7.7.4.4.5.5.4.4
 
 =cut
 
-our $VERSION = '1.6';
+our $VERSION = '1.7';
 
 has 'debug' => (
     is        => 'rw',
