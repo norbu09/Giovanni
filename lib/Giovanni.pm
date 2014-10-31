cc107020801c918b996085794e4344d467ddefde
Lenz Gschwendtner <lenz@springtimesoft.com>
2014-10-31 12:23:38 +0800

    fix version number

--- lib/Giovanni.pm
+++ lib/Giovanni.pm
@@ -16,11 +16,11 @@ Giovanni - a Perl based deployment system
 
 =head1 VERSION
 
-Version 1.8.7.7.4.4.5.5.4.4
+Version 1.9
 
 =cut
 
-our $VERSION = '1.8';
+our $VERSION = '1.9';
 
 has 'debug' => (
     is        => 'rw',
